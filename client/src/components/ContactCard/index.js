11eb4ef565ee4e04b936347bf02488134859018c
Ryan Goderis <rgoderis789@gmail.com>
2020-03-10 11:30:38 -0400

    in ContactCard set view more button to contain a link to /details with id of contact passed in as a param

--- client/src/components/ContactCard/index.js
+++ client/src/components/ContactCard/index.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react"
+import {Link} from "react-router-dom"
 
 function ContactCard(props){
     return(
         <div>
             <h2>{props.lastName}, {props.firstName}</h2>
             <h3>{props.address} {props.city}, {props.state}. {props.zip}</h3>
-            <button>View More</button>
+            <button><Link to={"/details/"+props.id}>View More</Link></button>
         </div>
     )
 }
