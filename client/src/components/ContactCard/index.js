846e4cbce5344851210e934e62fcc133f3d093fa
Ryan Goderis <rgoderis789@gmail.com>
2020-03-18 10:39:16 -0400

    In ContactCard added class of contact-card to div to style background-color, changed text color and style button to match style of previous buttons. Increased padding and margins of div

--- client/src/components/ContactCard/index.js
+++ client/src/components/ContactCard/index.js
@@ -1,13 +1,14 @@
 import React from "react"
 import {Link} from "react-router-dom"
+import "./style.css"
 
 
 function ContactCard(props){
     return(
-        <div>
-            <h2>{props.lastName}, {props.firstName}</h2>
-            <h3>{props.address} {props.city}, {props.state}. {props.zip}</h3>
-            <button><Link to={"/details/"+props.id}>View More</Link></button>
+        <div className="contact-card container rounded mt-2 mb-2 p-3">
+            <h2 className="text-white">{props.lastName}, {props.firstName}</h2>
+            <h3 className="text-white">{props.address} {props.city}, {props.state}. {props.zip}</h3>
+            <Link to={"/details/"+props.id} className="text-dark text-decoration-none btn btn-lg btn-info">View More</Link>
         </div>
     )
 }
