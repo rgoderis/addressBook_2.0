a63ef1cf5388efa8bdacf5400d86592c0d5926f2
Ryan Goderis <rgoderis789@gmail.com>
2020-04-08 13:01:05 -0400

    changed join in server.js file to ./client/build

---
+++ client/build/precache-manifest.48cf3b9c3a905991ed82157b365f6c6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "63c846ae3e185e86ef91",
+    "url": "/static/js/main.63c846ae.chunk.js"
+  },
+  {
+    "revision": "ee090a3722bf7409215d",
+    "url": "/static/js/1.ee090a37.chunk.js"
+  },
+  {
+    "revision": "63c846ae3e185e86ef91",
+    "url": "/static/css/main.62905cc3.chunk.css"
+  },
+  {
+    "revision": "a9db0070f3a6d200a2b4659a66f1f01b",
+    "url": "/index.html"
+  }
+];
